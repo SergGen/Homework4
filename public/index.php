a43ca85b93cc741ba44bfe808d9a9181b58477b2
sergey_p@mail.ru <1Z2x-3C4v>
2021-02-05 19:44:57 +0300

    ДЗ 4

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once __DIR__ . "/../config/constants.php";
+
+include ENGINE_DIR . "functions.php";
+
+$images = getFiles (PUBLIC_DIR . "img");
+
+include HTML_DIR . "html.php";
+
+
+
